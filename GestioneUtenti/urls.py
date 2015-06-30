253d2672f368c73d208b0e15f0603b4a496385e4
Marco Torreggiani <IlTorre>
2015-06-30 16:48:13 +0200

    Implementato registrazione nuovi utenti, sistema di attivazione

--- GestioneUtenti/urls.py
+++ GestioneUtenti/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
+
+
 
 urlpatterns = [
     url(r'^$', views.portal_main_page, name='profilo'),
     url(r'^login/$', views.mylogin, name='login'),
     url(r'^logout/$', views.mylogout, name='logout'),
+    url(r'^registrazione/$',views.registrazione, name='registrazione'),
+    url(r'^attiva/(?P<id_utente>[0-9]+)/$',views.attiva, name='attivazione')
+
     ]
--- GestioneUtenti/views.py
+++ GestioneUtenti/views.py
@@ -3,9 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import HttpResponseRedirect
-
-# Create your views here.
-
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 def mylogin(request):
     if request.method =='POST':
@@ -26,6 +25,28 @@ def mylogin(request):
         else:
             return HttpResponseRedirect('/account')
 
+def registrazione(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password1']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        try:
+            user = User.objects.create_user(username=username,email=email,password=password,first_name=first_name,last_name=last_name)
+            user.is_active=False
+            user.save()
+            info={'titolo':'Utente registrato correttamente','corpo':'Verifica la mail per attivare il tuo account'}
+            return render(request, 'GestioneUtenti/avviso.html',info)
+        except IntegrityError:
+            utente = {'first_name': first_name,'last_name': last_name,'email': email,'messaggio':'Attenzione username non valido'}
+            return render(request,'GestioneUtenti/registrazione.html',utente)
+    else:
+        if request.user.is_anonymous():
+            return render(request,'GestioneUtenti/registrazione.html')
+        else:
+            return HttpResponseRedirect('/account')
+
 def mylogout (request):
     logout(request)
     return render(request,'GestioneUtenti/login.html',{'messaggio':'Logout eseguito correttamente.'})
@@ -34,3 +55,11 @@ def mylogout (request):
 @login_required(login_url='/account/login')
 def portal_main_page(request):
     return render_to_response('GestioneUtenti/profilo.html',{'request':request})
+
+def attiva(request, id_utente):
+    user = User.objects.get(pk=id_utente)
+    user.is_active=True
+    user.save()
+
+    info={'titolo':'Account attivato con successo','corpo':'Da adesso in poi puoi navigare liberamente'}
+    return render(request,'GestioneUtenti/avviso.html',info)
