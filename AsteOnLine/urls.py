a106cc58534e346168a2811e448700a840a2cdd0
Marco Torreggiani <IlTorre>
2015-06-30 12:29:58 +0200

    Implementato logout, sistemati decoratori login required nelle categorie, impedito di rifare il login a utenti già loggati, implementati messaggi di feedback

--- AsteOnLine/urls.py
+++ AsteOnLine/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$', views.Home.as_view(), name='index'),
     url(r'^asta(?P<pk>[0-9]+)/$', views.Dettaglio.as_view(), name='dettaglio'),
-    url(r'^categorie$', views.Categorie.as_view(), name ='categorie'),
+    url(r'^categorie$', login_required(views.Categorie.as_view(), login_url='/account/login'), name ='categorie'),
 ]
